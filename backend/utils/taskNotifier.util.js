6fb35a001423c8c1557d932210d586221c4ee49e
Vibhab-T <vibhabtimsina86@gmail.com>
2025-06-11 16:21:38 +0545

    made changes to email logic

--- backend/utils/taskNotifier.util.js
+++ backend/utils/taskNotifier.util.js
@@ -1,24 +1,21 @@
 import cron from 'node-cron';
 import Project from '../models/projects.model.js';
-import Task from '../models/tasks.model.js';
-import User from '../models/users.model.js';
 import resend from './resendClient.util.js';
 
-cron.schedule('*/20 * * * *', async () => {
-	console.log('Running Twenty Minute Notification Check');
+cron.schedule('*/2 * * * *', async () => {
+	console.log('Running Two Minute Notification Check');
 
 	try {
-		const twentyMinsAgo = new Date(Date.now() - 20 * 60 * 1000);
-
 		const projects = await Project.find({}).populate('users').populate('tasks');
-
 		for (const project of projects) {
+			const lastNotifiedAt = project.lastNotifiedAt || new Date(0);
+
 			const changedTasks = project.tasks.filter(
 				(task) =>
-					task.updatedAt > twentyMinsAgo || task.createdAt > twentyMinsAgo
+					task.updatedAt > lastNotifiedAt || task.createdAt > lastNotifiedAt
 			);
 
-			if (changedTasks.length === 0) continue;
+			if (changedTasks.length == 0) continue;
 
 			const message = changedTasks
 				.map((task) => `- ${task.name} (${task.category})\n`)
@@ -32,15 +29,17 @@ cron.schedule('*/20 * * * *', async () => {
 						subject: `Task Update In ${project.projectName}`,
 						text: `Hello, ${user.fullname}\nThere are recent changes in "${project.projectName}":\n\n${message}\n\nCheers,\nKanPlan Team`,
 					});
-				} catch (emailError) {
-					console.error(
-						`Failed to send email to ${user.email}:`,
-						emailError.message
-					);
+				} catch (error) {
+					console.log(`Error sending email to ${user.email}\n`, error.message);
 				}
 			}
+
+			//update lastNotifiedAt
+			project.lastNotifiedAt = new Date();
+			await project.save();
+
+			console.log('Emails Sent');
 		}
-		console.log('Emails Sent');
 	} catch (error) {
 		console.error('Error running notification cron:', error.message);
 	}
